c3a807b7eeae16584843722d72b37377ac5b9717
samanfathnazarian <saman.nazarian17@gmail.com>
2022-11-15 01:57:50 -0600

    Implemented getEvent() in viewsController
    Also added the required PUG, HTML & CSS  files to render the event.

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,18 @@
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const Event = require('../models/eventModel');
 
 exports.getHomepage = catchAsync(async (req, res, next) => {});
+
+exports.getEvent = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const event = await Event.findById(req.params.id).populate('participants').select('-__v');
+
+  // 2) Build & Render template using tour data from 1
+  res.status(200).render('event', {
+    title: event.name,
+    event: event,
+  });
+});
+
 exports.createEvent = catchAsync(async (req, res, next) => {});
-exports.getEvent = catchAsync(async (req, res, next) => {});
