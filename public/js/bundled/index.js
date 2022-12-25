03a4508ddac5ca52a2d4a5dd43c8aac84f212c26
samanfathnazarian <saman.nazarian17@gmail.com>
2022-12-24 23:50:44 -0600

    Implemented & styled the create-participant component
    Also, added transitions to the invite-respond when the close button on it is clicked.

--- public/js/bundled/index.js
+++ public/js/bundled/index.js
@@ -4393,8 +4393,14 @@ function clipboardCopy() {
 },{"../utilities/alerts":"jSl5m","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"a78JO":[function(require,module,exports) {
 const inviteEl = document.querySelector(".invite");
 const inviteArrowBtnEl = document.querySelector("#invite__arrow-btn");
+const arrowOpenEl = document.querySelector(".arrow-open");
 inviteArrowBtnEl.addEventListener("click", ()=>{
     inviteEl.classList.toggle("invite--hide");
+    arrowOpenEl.classList.toggle("arrow-open--hide");
+});
+arrowOpenEl.addEventListener("click", ()=>{
+    inviteEl.classList.toggle("invite--hide");
+    arrowOpenEl.classList.toggle("arrow-open--hide");
 });
 
 },{}]},["f2QDv"], "f2QDv", "parcelRequire1761")
--- public/js/getEvent/index.js
+++ public/js/getEvent/index.js
@@ -1,6 +1,13 @@
 const inviteEl = document.querySelector('.invite');
 const inviteArrowBtnEl = document.querySelector('#invite__arrow-btn');
+const arrowOpenEl = document.querySelector('.arrow-open');
 
 inviteArrowBtnEl.addEventListener('click', () => {
   inviteEl.classList.toggle('invite--hide');
+  arrowOpenEl.classList.toggle('arrow-open--hide');
+});
+
+arrowOpenEl.addEventListener('click', () => {
+  inviteEl.classList.toggle('invite--hide');
+  arrowOpenEl.classList.toggle('arrow-open--hide');
 });
