16da72f730ab0b7f08cf85f176ede795f0b46efa
samanfathnazarian <saman.nazarian17@gmail.com>
2022-12-25 23:35:52 -0600

    Implemented functionality of the invite respond component.
    For example, whenever the user clicks on the confirm invite button, the create-participant form is shown.

--- public/js/bundled/index.js
+++ public/js/bundled/index.js
@@ -4391,6 +4391,8 @@ function clipboardCopy() {
 }
 
 },{"../utilities/alerts":"jSl5m","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"a78JO":[function(require,module,exports) {
+var _alerts = require("../utilities/alerts");
+// Opening & closing the invite
 const inviteEl = document.querySelector(".invite");
 const inviteArrowBtnEl = document.querySelector("#invite__arrow-btn");
 const arrowOpenEl = document.querySelector(".arrow-open");
@@ -4402,7 +4404,32 @@ arrowOpenEl.addEventListener("click", ()=>{
     inviteEl.classList.toggle("invite--hide");
     arrowOpenEl.classList.toggle("arrow-open--hide");
 });
+const createParticipantSubmitEl = document.querySelector(".create-participant__submit");
+const createParticipantCancelEL = document.querySelector(".create-participant__cancel");
+const overlayEl = document.querySelector(".overlay");
+createParticipantSubmitEl.addEventListener("click", (e)=>{
+    e.preventDefault();
+    (0, _alerts.showError)("Functionality not implemented!");
+// overlayEl.classList.add('u-display-hide');
+});
+createParticipantCancelEL.addEventListener("click", (e)=>{
+    e.preventDefault();
+    overlayEl.classList.add("u-display-hide");
+});
+overlayEl.addEventListener("click", (e)=>{
+    if (e.target.classList.contains("overlay")) overlayEl.classList.add("u-display-hide");
+});
+const inviteConfirmBtnEl = document.querySelector("#invite__confirm-btn");
+const inviteDeclineBtnEl = document.querySelector("#invite__decline-btn");
+inviteConfirmBtnEl.addEventListener("click", (e)=>{
+    e.preventDefault();
+    overlayEl.classList.remove("u-display-hide");
+});
+inviteDeclineBtnEl.addEventListener("click", (e)=>{
+    e.preventDefault();
+    overlayEl.classList.remove("u-display-hide");
+});
 
-},{}]},["f2QDv"], "f2QDv", "parcelRequire1761")
+},{"../utilities/alerts":"jSl5m"}]},["f2QDv"], "f2QDv", "parcelRequire1761")
 
 //# sourceMappingURL=index.js.map
--- public/js/getEvent/index.js
+++ public/js/getEvent/index.js
@@ -1,3 +1,6 @@
+import { showError } from '../utilities/alerts';
+
+// Opening & closing the invite
 const inviteEl = document.querySelector('.invite');
 const inviteArrowBtnEl = document.querySelector('#invite__arrow-btn');
 const arrowOpenEl = document.querySelector('.arrow-open');
@@ -10,4 +13,36 @@ inviteArrowBtnEl.addEventListener('click', () => {
 arrowOpenEl.addEventListener('click', () => {
   inviteEl.classList.toggle('invite--hide');
   arrowOpenEl.classList.toggle('arrow-open--hide');
+});
+
+const createParticipantSubmitEl = document.querySelector('.create-participant__submit');
+const createParticipantCancelEL = document.querySelector('.create-participant__cancel');
+const overlayEl = document.querySelector('.overlay');
+
+createParticipantSubmitEl.addEventListener('click', (e) => {
+  e.preventDefault();
+  showError('Functionality not implemented!');
+  // overlayEl.classList.add('u-display-hide');
+});
+
+createParticipantCancelEL.addEventListener('click', (e) => {
+  e.preventDefault();
+  overlayEl.classList.add('u-display-hide');
+});
+
+overlayEl.addEventListener('click', (e) => {
+  if (e.target.classList.contains('overlay')) overlayEl.classList.add('u-display-hide');
+});
+
+const inviteConfirmBtnEl = document.querySelector('#invite__confirm-btn');
+const inviteDeclineBtnEl = document.querySelector('#invite__decline-btn');
+
+inviteConfirmBtnEl.addEventListener('click', (e) => {
+  e.preventDefault();
+  overlayEl.classList.remove('u-display-hide');
+});
+
+inviteDeclineBtnEl.addEventListener('click', (e) => {
+  e.preventDefault();
+  overlayEl.classList.remove('u-display-hide');
 });
